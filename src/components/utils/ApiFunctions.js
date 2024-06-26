2f5da90e36e3a3f9c1f2eff986c3d522601f05ab
codexmalinga <rdimalingadesilva@gmail.com>
2024-06-26 16:54:58 +0530

    changed back

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -1,7 +1,14 @@
 import axios from "axios"
 
+// export const api = axios.create({
+// 	baseURL: "https://luckily-humble-gobbler.ngrok-free.app",
+// 	headers: {
+//         "Content-Type": "application/json"
+//     }
+// })
+
 export const api = axios.create({
-	baseURL: "https://luckily-humble-gobbler.ngrok-free.app",
+	baseURL: "http://localhost:9192",
 	headers: {
         "Content-Type": "application/json"
     }
